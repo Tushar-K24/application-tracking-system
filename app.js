5c437ddacddeee58b8e3617232b1456125992cb1
Tushar Khanduri <62638544+Tushar-K24@users.noreply.github.com>
2023-06-13 15:03:35 +0530

    refactor: api version

--- app.js
+++ app.js
@@ -4,16 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
-const organizationAuthRouter = require("./src/routers/organization/organization.authRouter");
-const organizationQueryRouter = require("./src/routers/organization/organization.queryRouter");
-const organizationJobRouter = require("./src/routers/organization/organization.jobRouter");
-const organizationApplicantRouter = require("./src/routers/organization/organization.applicantRouter");
-
-const applicantAuthRouter = require("./src/routers/applicant/applicant.authRouter");
-const applicantQueryRouter = require("./src/routers/applicant/applicant.queryRouter");
-const applicantJobRouter = require("./src/routers/applicant/applicant.jobRouter");
-
-const jobRouter = require("./src/routers/commons/jobRouter");
+const apiV1 = require("./src/api/apiV1");
 
 const app = express();
 
@@ -39,16 +30,7 @@ mongoose
 
 mongoose.set("debug", true);
 
-app.use("/api/v1", jobRouter);
-
-app.use("/api/v1/organization/auth", organizationAuthRouter);
-app.use("/api/v1/organization", organizationQueryRouter);
-app.use("/api/v1/organization", organizationJobRouter);
-app.use("/api/v1/organization", organizationApplicantRouter);
-
-app.use("/api/v1/applicant/auth", applicantAuthRouter);
-app.use("/api/v1/applicant", applicantQueryRouter);
-app.use("/api/v1/applicant", applicantJobRouter);
+app.use("/api/v1", apiV1);
 
 app.listen(3000, () => {
   console.log("server started at port 3000");
